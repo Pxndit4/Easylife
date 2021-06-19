86a0816779bde1acc03faba95802dce9e018c4fa
Giancarlo Tueros <gtueros.developer@outlook.com>
2021-06-19 12:11:21 -0500

    Web Api Config

---
+++ UNCDF/Gastos Financieros/WebApis/UNCDF.WebApi.Config/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace UNCDF.WebApi.Config
+{
+    public class AppSettings
+    {
+        public string rootPath { get; set; }
+        public string LanguagePath { get; set; }
+        public string CountryPath { get; set; }
+        public string BannerPath { get; set; }
+        public string IntroductionPath { get; set; }
+    }
+}
---
+++ UNCDF/Gastos Financieros/WebApis/UNCDF.WebApi.Config/Startup.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using UNCDF.Layers.Model;
+
+namespace UNCDF.WebApi.Config
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        readonly string CorsPolicy = "_corsPolicy";
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.Configure<AppSettings>(Configuration);
+
+            services.Configure<MAwsEmail>(Configuration.GetSection("MAwsEmail"));
+
+            services.Configure<MAwsS3>(Configuration.GetSection("MAwsS3"));
+
+            services.Configure<FormOptions>(x =>
+            {
+                x.ValueLengthLimit = int.MaxValue;
+                x.MultipartBodyLengthLimit = int.MaxValue; // if don't set default value is: 128 MB
+                x.MultipartHeadersLengthLimit = int.MaxValue;
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: CorsPolicy,
+                                  builder =>
+                                  {
+                                      //builder.WithOrigins("");
+                                      builder.AllowAnyOrigin()
+                                       .AllowAnyMethod()
+                                       .AllowAnyHeader();
+                                  });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors(CorsPolicy);
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
