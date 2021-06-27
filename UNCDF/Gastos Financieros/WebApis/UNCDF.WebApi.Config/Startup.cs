8b09be346f1458f52ca503622334d06edf99218b
Miller Calla <millercallamoreyra@hotmail.com>
2021-06-26 19:41:33 -0500

    Se sube Referencias

--- UNCDF/Gastos Financieros/WebApis/UNCDF.WebApi.Config/Startup.cs
+++ UNCDF/Gastos Financieros/WebApis/UNCDF.WebApi.Config/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using UNCDF.Layers.Model;
